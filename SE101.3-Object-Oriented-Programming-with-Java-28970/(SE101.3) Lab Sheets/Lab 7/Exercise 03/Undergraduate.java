bcfe11525d2562a2d5590425a81e8aa723ecc13f
Tharuka Thilakarathna <139436606+thilakarathnaMTM@users.noreply.github.com>
2023-07-30 16:43:27 +0530

    Add files via upload

---
+++ SE101.3-Object-Oriented-Programming-with-Java-28970/(SE101.3) Lab Sheets/Lab 7/Exercise 03/Undergraduate.java
@@ -0,0 +1,4 @@
+//Student class cannot be utilised by another subclass that attempts to inherit it since it is declared with the final modifier.
+
+// ERROR:
+public class Undergraduate extends Student {}
